2b7ce2840cd4513ccd73f3c23f35cdcc53ba557f
Courtney Jackson <courtneyjackson@Courtneys-Air.lan>
2021-08-02 22:33:38 -0700

    organizing first page

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const DOMAIN = 'http://www.omdbapi.com/';
-const API_KEY = '61af40b2'
-const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&s=`;
+const DOMAIN = 'https://opentdb.com/api.php?amount=10&category=32&difficulty=medium&type=multiple&encode=url3986'
+
+// const API_KEY = '61af40b2'
+// const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&s=`;
+
+console.log(DOMAIN)
+
+const fetchedTrivia = async () => {
+  
+}
