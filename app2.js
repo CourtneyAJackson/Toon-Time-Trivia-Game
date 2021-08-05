74573284276a17a0ffbed73a2cd6db3702f21d55
Courtney Jackson <courtneyjackson@Courtneys-MacBook-Air.local>
2021-08-04 20:20:22 -0700

    stuck on moving to next question

--- app2.js
+++ app2.js
@@ -4,8 +4,8 @@
 const DOMAIN = "https://opentdb.com/api.php?amount=10&category=32&difficulty=medium&type=multiple"
 
 
-  
-  // console.log(DOMAIN)
+
+// console.log(DOMAIN)
 
 //console.log(mcAnswers)
 //const mcQuestion = document.getElementById("mc-questions")
@@ -20,68 +20,116 @@ const getData = async () => {
   try {
     const data = await axios.get(DOMAIN)
     const dataInfo = data.data.results
-  //eachQuestion = () => {
+    //console.log(dataInfo)
+    //console.log(dataInfo)
+    //eachQuestion = () => {
     dataInfo.forEach(data => {
       //console.log(data.question)
-      let questions = data.question
-      newArray.push(questions)
+      //let questions =
+        //localStorage.setItem("dataInfo", "question") 
+      //console.log(questions)
+      newArray.push(data.question)
+      //console.log(newArray.length)
       //const availableQuestions = [questions]
       //console.log(availableQuestions)
       //console.log(questions)
-    })
-   applyDataRetrieved(dataInfo)
+      // for (let i = 0; i < questions.length; i++){
+      //   console.log(questions)
+      //newGame(questions[i])
 
-   
-  
+
+
+    })
+    //console.log(questions)
+    applyDataRetrieved(dataInfo)
+
+    // newGame(questions)
+
+
   } catch (error) {
-     console.log(error)
-   }
+    console.log(error)
+  }
 }
-  
+
 getData()
 //console.log(newArray)
+// const dataInfo = localStorage.getItem("dataInfo.question")
+// console.log(dataInfo)
+
+console.log(newArray.length)
+//console.log(newArray)
+//console.log(dataArray)
+let scoreContainer = document.querySelector(".players-score")
+//let numOfQuestions = newArray.length
+let correctAnswer = document.getElementById("choice4")
+newGame = () => {
+  let currentQuestionCount = 0
+  let score = 0
+  let clickCount = 11
+  //console.log(newArray)
 
 
-// newGame = () => {
-//   const mcChoices = Array.from(document.getElementsByClassName("button"))
-//   console.log(mcChoices)
-//   mcChoices.forEach(mcChoice => {
-//     mcChoice.addEventListener("click", event => {
-//       console.log("hello")
-   
-  
-    // shuffledQuestions = questions.sort(() => Math.floor(Math.random() * 3))
-    // currentQuestionIdx = 0
 
-  //     if (mcChoice === correctAnswer) {
-  //       numOfQuestions--
-  //       alert(`Correct! ${numOfQuestions} to go!`)
-  //       window.location.reload()
-  //       score++
-  //       currentQuestionCount ++
-  //     } else
-  //       numOfQuestions --
-  //       alert(`UhOh, wrong answer! Only${numOfQuestions} questions remaining`)
-  //       window.location.reload()
-  //       currentQuestionCount ++
-  //    //setNextQuestion()
-  //     if (score >= 10) {
-  //       alert("Congratulations!!! You Win!")
-  //     } else {
-  //       ("Maybe next time")
-  //     }
-     
-  //    })
-  // })
-  
+  let currentScore = document.querySelector(".score")
+
+  scoreContainer.append(currentScore)
+
+  const mcChoices = Array.from(document.querySelectorAll(".button"))
+  console.log(mcChoices)
+
+
+
+  mcChoices.forEach(mcChoice => {
+
+    mcChoice.addEventListener("click", event => {
+      if (score === 10) {
+        alert("Coongratulations you won!")
+        window.location.assign("./index.html")
+      } else
+        if (numOfQuestions === 1) {
+          alert("Sorry. Game Over")
+          window.location.assign("./index.html")
+        } else
+        if (mcChoice === correctAnswer) {
+          numOfQuestions = numOfQuestions - 1
+          score++
+          
+          //window.location.reload()
+          currentScore.innerHTML = `Score: ${score}`
+          alert(`Correct! ${numOfQuestions} questions to go!`)
+          //window.location.reload()
+          currentQuestionCount++
+          
+        } else
+          if (mcChoice !== correctAnswer) {
+            numOfQuestions = numOfQuestions - 1
+            alert(`Uh Oh, wrong answer! Only ${numOfQuestions} questions remaining`)
+            currentQuestionCount++
+            clickCount++
+          }
+
+      //setNextQuestion()
+      // if (score >= 10) {
+      //   alert("Congratulations!!! You Win!")
+      // } else {
+      //   ("Maybe next time")
+      //} // }
+
+
+    })
+
+  })
+
+
+
   //return score
-// }
-// newGame()
+}
+newGame()
 
 // const mcAnswers = document.querySelectorAll(".button")
 // console.log(mcAnswers)
 // reset = (mcAnswers) => {
-  
+
 // }
 
 
@@ -89,28 +137,40 @@ getData()
 //async function to fetch the data we need from API
 
 //}
-    //eachQuestion()
+//eachQuestion()
 //console.log(data.data.results)
-    //console.log(data.data.results)
+//console.log(data.data.results)
 //console.log(questions)
 // function to access data we get back
-  
 
 
-const questions = newArray
-console.log(questions) 
- let acceptAmswers = true;
-// currentQuestionCount = 0
-// numOfQuestions = 10
-let score = 0
 
-// getNextQuestion = () => {
-//   if (questions.length === 0 {
-//     localStorage.setItem()
-    
-//   }
-// }
+//const questions = newArray
+//console.log(questions) 
+let acceptAmswers = true;
+let currentQuestionCount = 0
+let numOfQuestions = 10
+let currentQuestionIndex = 0
 
+
+
+getNextQuestion = () => {
+  if (numOfQuestions.length === 0 || numOfQuestions === 10) {
+    localStorage.setItem("mostRecentScore", score)
+
+
+    //return window.location.assign("/.index.html")
+  }
+  numOfQuestions--
+
+  currentQuestionIndex = Math.floor(Math.random() * numOfQuestions.length)
+  currentQuestion = numOfQuestions[currentQuestionIndex]
+}
+getNextQuestion()
+//console.log(numOfQuestions)
+
+// shuffledQuestions = questions.sort(() => Math.floor(Math.random() * 3))
+// currentQuestionIdx = 0
 
 function applyDataRetrieved(dataInfo) {
   document.querySelector("#mc-questions").innerHTML = `${dataInfo[0].question}`
@@ -182,7 +242,7 @@ applyDataRetrieved()
 // let correctButton = document.querySelector("#choice4")
 // correctButton.addEventListener("click", () => {
 //   alert("You are correct!")
-  
+
 // })
 
 // const choice1 = document.getElementById("#choice1")
