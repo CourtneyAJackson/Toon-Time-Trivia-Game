5159983f883bd4e41bdd5f857296dc54cd87e936
Courtney Jackson <courtneyjackson@Courtneys-MacBook-Air.local>
2021-08-04 17:13:47 -0700

    mc functionality

--- app.js
+++ app.js
@@ -7,7 +7,26 @@ const DOMAIN = 'https://opentdb.com/api.php?amount=10&category=32&difficulty=med
 
   
 document.addEventListener("click", (event) => {
-  alert("hello")
+  alert("Good Luck!")
 })
 
 
+// questions = dataQuestions.map(dataQuestion => {
+      
+//   const formattedQuestion = {
+//     question: dataQuestion.question
+//   }
+//     //console.log(formattedQuestion)
+
+//     const answers = [...dataQuestion.incorrect_answers]
+//     formattedQuestion.answer = Math.floor(Math.random() * 3) + 1
+//     answers.splice(formattedQuestion.answer - 1,
+//       0,
+//       dataQuestion.correct_answer
+//     )
+//     //console.log(answers)
+//     answers.forEach((answer, i) => {
+//       formattedQuestion["answer" + (i + 1)] = answer;
+//     })
+//     return formattedQuestion  
+//  })
--- app2.js
+++ app2.js
@@ -1,42 +1,190 @@
 
+// window.addEventListener("DOMContenetLoaded", (event)=> { 
+
+const DOMAIN = "https://opentdb.com/api.php?amount=10&category=32&difficulty=medium&type=multiple"
 
 
-const DOMAIN = 'https://opentdb.com/api.php?amount=10&category=32&difficulty=medium&type=multiple&encode=url3986'
+  
+  // console.log(DOMAIN)
 
-//console.log(DOMAIN)
+//console.log(mcAnswers)
+//const mcQuestion = document.getElementById("mc-questions")
+let newArray = []
+// let currQuestion = {};
+// //let acceptingAswers = false;
+//let score = 0;
+// let questionCount;
+// let questions = []
+//const currentQuestionIndex
+const getData = async () => {
+  try {
+    const data = await axios.get(DOMAIN)
+    const dataInfo = data.data.results
+  //eachQuestion = () => {
+    dataInfo.forEach(data => {
+      //console.log(data.question)
+      let questions = data.question
+      newArray.push(questions)
+      //const availableQuestions = [questions]
+      //console.log(availableQuestions)
+      //console.log(questions)
+    })
+   applyDataRetrieved(dataInfo)
 
-// const triviaData = async () => {
-//   try {
-//     const response = await axios.get(DOMAIN)
-//     console.log(response)
-//   }
-//   catch (error) {
-//     console.log("error", error)
+   
+  
+  } catch (error) {
+     console.log(error)
+   }
+}
+  
+getData()
+//console.log(newArray)
+
+
+// newGame = () => {
+//   const mcChoices = Array.from(document.getElementsByClassName("button"))
+//   console.log(mcChoices)
+//   mcChoices.forEach(mcChoice => {
+//     mcChoice.addEventListener("click", event => {
+//       console.log("hello")
+   
+  
+    // shuffledQuestions = questions.sort(() => Math.floor(Math.random() * 3))
+    // currentQuestionIdx = 0
+
+  //     if (mcChoice === correctAnswer) {
+  //       numOfQuestions--
+  //       alert(`Correct! ${numOfQuestions} to go!`)
+  //       window.location.reload()
+  //       score++
+  //       currentQuestionCount ++
+  //     } else
+  //       numOfQuestions --
+  //       alert(`UhOh, wrong answer! Only${numOfQuestions} questions remaining`)
+  //       window.location.reload()
+  //       currentQuestionCount ++
+  //    //setNextQuestion()
+  //     if (score >= 10) {
+  //       alert("Congratulations!!! You Win!")
+  //     } else {
+  //       ("Maybe next time")
+  //     }
+     
+  //    })
+  // })
+  
+  //return score
+// }
+// newGame()
+
+// const mcAnswers = document.querySelectorAll(".button")
+// console.log(mcAnswers)
+// reset = (mcAnswers) => {
+  
+// }
+
+
+
+//async function to fetch the data we need from API
+
+//}
+    //eachQuestion()
+//console.log(data.data.results)
+    //console.log(data.data.results)
+//console.log(questions)
+// function to access data we get back
+  
+
+
+const questions = newArray
+console.log(questions) 
+ let acceptAmswers = true;
+// currentQuestionCount = 0
+// numOfQuestions = 10
+let score = 0
+
+// getNextQuestion = () => {
+//   if (questions.length === 0 {
+//     localStorage.setItem()
+    
 //   }
 // }
 
-// axios.get(DOMAIN).then(response => {
-//    console.log(response)
-//  })
-    
-  
-//window.onload = getData
 
-// async function to fetch the data we need from API
-const getData = async () => {
-  try{
-    const response = await axios.get(DOMAIN)
-    console.log(response)
-    //applyDataRetrieved(response)
-  } catch (error) {
-    console.log(error)
- }
+function applyDataRetrieved(dataInfo) {
+  document.querySelector("#mc-questions").innerHTML = `${dataInfo[0].question}`
+  document.querySelector("#mc-questions").innerHTML = `${dataInfo[1].question}`
+  document.querySelector("#mc-questions").innerHTML = `${dataInfo[2].question}`
+  document.querySelector("#mc-questions").innerHTML = `${dataInfo[3].question}`
+  document.querySelector("#mc-questions").innerHTML = `${dataInfo[4].question}`
+  document.querySelector("#mc-questions").innerHTML = `${dataInfo[5].question}`
+  document.querySelector("#mc-questions").innerHTML = `${dataInfo[6].question}`
+  document.querySelector("#mc-questions").innerHTML = `${dataInfo[7].question}`
+  document.querySelector("#mc-questions").innerHTML = `${dataInfo[8].question}`
+  document.querySelector("#mc-questions").innerHTML = `${dataInfo[9].question}`
+
+
+  document.querySelector("#choice1").innerHTML = `${dataInfo[0].incorrect_answers[0]}`
+  document.querySelector("#choice2").innerHTML = `${dataInfo[0].incorrect_answers[1]}`
+  document.querySelector("#choice3").innerHTML = `${dataInfo[0].incorrect_answers[2]}`
+  document.querySelector("#choice4").innerHTML = `${dataInfo[0].correct_answer}`
+
+  document.querySelector("#choice1").innerHTML = `${dataInfo[1].incorrect_answers[0]}`
+  document.querySelector("#choice2").innerHTML = `${dataInfo[1].incorrect_answers[1]}`
+  document.querySelector("#choice3").innerHTML = `${dataInfo[1].incorrect_answers[2]}`
+  document.querySelector("#choice4").innerHTML = `${dataInfo[1].correct_answer}`
+
+  document.querySelector("#choice1").innerHTML = `${dataInfo[2].incorrect_answers[0]}`
+  document.querySelector("#choice2").innerHTML = `${dataInfo[2].incorrect_answers[1]}`
+  document.querySelector("#choice3").innerHTML = `${dataInfo[2].incorrect_answers[2]}`
+  document.querySelector("#choice4").innerHTML = `${dataInfo[2].correct_answer}`
+
+  document.querySelector("#choice1").innerHTML = `${dataInfo[3].incorrect_answers[0]}`
+  document.querySelector("#choice2").innerHTML = `${dataInfo[3].incorrect_answers[1]}`
+  document.querySelector("#choice3").innerHTML = `${dataInfo[3].incorrect_answers[2]}`
+  document.querySelector("#choice4").innerHTML = `${dataInfo[3].correct_answer}`
+
+  document.querySelector("#choice1").innerHTML = `${dataInfo[4].incorrect_answers[0]}`
+  document.querySelector("#choice2").innerHTML = `${dataInfo[4].incorrect_answers[1]}`
+  document.querySelector("#choice3").innerHTML = `${dataInfo[4].incorrect_answers[2]}`
+  document.querySelector("#choice4").innerHTML = `${dataInfo[4].correct_answer}`
+
+  document.querySelector("#choice1").innerHTML = `${dataInfo[5].incorrect_answers[0]}`
+  document.querySelector("#choice2").innerHTML = `${dataInfo[5].incorrect_answers[1]}`
+  document.querySelector("#choice3").innerHTML = `${dataInfo[5].incorrect_answers[2]}`
+  document.querySelector("#choice4").innerHTML = `${dataInfo[5].correct_answer}`
+
+  document.querySelector("#choice1").innerHTML = `${dataInfo[6].incorrect_answers[0]}`
+  document.querySelector("#choice2").innerHTML = `${dataInfo[6].incorrect_answers[1]}`
+  document.querySelector("#choice3").innerHTML = `${dataInfo[6].incorrect_answers[2]}`
+  document.querySelector("#choice4").innerHTML = `${dataInfo[6].correct_answer}`
+
+  document.querySelector("#choice1").innerHTML = `${dataInfo[7].incorrect_answers[0]}`
+  document.querySelector("#choice2").innerHTML = `${dataInfo[7].incorrect_answers[1]}`
+  document.querySelector("#choice3").innerHTML = `${dataInfo[7].incorrect_answers[2]}`
+  document.querySelector("#choice4").innerHTML = `${dataInfo[7].correct_answer}`
+
+  document.querySelector("#choice1").innerHTML = `${dataInfo[8].incorrect_answers[0]}`
+  document.querySelector("#choice2").innerHTML = `${dataInfo[8].incorrect_answers[1]}`
+  document.querySelector("#choice3").innerHTML = `${dataInfo[8].incorrect_answers[2]}`
+  document.querySelector("#choice4").innerHTML = `${dataInfo[8].correct_answer}`
+
+  document.querySelector("#choice1").innerHTML = `${dataInfo[9].incorrect_answers[0]}`
+  document.querySelector("#choice2").innerHTML = `${dataInfo[9].incorrect_answers[1]}`
+  document.querySelector("#choice3").innerHTML = `${dataInfo[9].incorrect_answers[2]}`
+  document.querySelector("#choice4").innerHTML = `${dataInfo[9].correct_answer}`
 
 }
-getData()
+applyDataRetrieved()
 
+//document.addEventListener("")
+// let correctButton = document.querySelector("#choice4")
+// correctButton.addEventListener("click", () => {
+//   alert("You are correct!")
+  
+// })
 
-//function applyDataRetrieved(response) {
- // document.querySelectorAll("#mc-questions").innerHTML = `Question: ${response.result[0].question}`
-//}
-//applyDataRetrieved()
+// const choice1 = document.getElementById("#choice1")
+// console.log(choice1)
+
